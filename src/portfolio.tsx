41cbe93cbc48d4169b08a6f104ea7b180d26d151
mkorrea <mathscorrea@gmail.com>
2024-08-23 18:58:51 -0300

    adjust photo for mobile and add favicon

--- src/portfolio.tsx
+++ src/portfolio.tsx
@@ -5,33 +5,33 @@ import homePhoto from './assets/photo-home.png'
 export function Portfolio() {
   return (
     <div className="w-screen h-screen flex justify-center overflow-x-hidden">
-      <main className="w-[1920px] max-w-screen border-l-2 border-r-2 border-border ">
+      <main className="w-[1920px] max-w-screen border-l-2 border-r-2 border-border px-3 ">
         
 
         {/* home */}
-        <div className='h-screen flex '>
+        <div className=' h-screen flex flex-col justify-end '>
 
-          <div className='flex my-auto w-screen h-fit '>
+          <div className='relative w-full h-screen flex justify-center align-middle'>
             {/* presentation */}
-            <div className='flex flex-col justify-center mx-auto gap-y-4 z-0'>
-              <p className='text-zinc-400 text-3xl'>
+            <div className='absolute mt-36 h-fit flex flex-col justify-center mx-auto gap-y-2 z-0  '>
+              <p className='text-zinc-400 text-base'>
                 Olá! Eu me chamo
               </p>
-              <h1 className='flex text-9xl font-bold text-nowrap bg-gradient-to-r from-blue-400 via-indigo-500 to-indigo-600 text-transparent bg-clip-text '>
+              <h1 className='flex text-4xl font-bold text-nowrap bg-gradient-to-r from-blue-400 via-indigo-500 to-indigo-600 text-transparent bg-clip-text '>
                 Matheus Correa
               </h1>
-              <h1 className='absolute flex text-9xl font-bold text-nowrap bg-gradient-to-r from-blue-400 via-indigo-500 to-indigo-600 text-transparent bg-clip-text text-shadow blur-md  '>
+              <h1 className='absolute flex text-4xl font-bold text-nowrap bg-gradient-to-r from-blue-400 via-indigo-500 to-indigo-600 text-transparent bg-clip-text text-shadow blur-md  '>
                 Matheus Correa
               </h1>
-              <p className='text-zinc-100 text-2xl text-center font-bold italic tracking-widest'>
+              <p className='text-zinc-100 text-base text-center font-bold italic tracking-wider'>
                 Desenvolvedor Front-end
               </p>
             </div>
 
             {/* image */}
-            <div className="flex w-2/5 align-bottom justify-end">
+            <div className="flex align-bottom justify-end mt-auto w-full h-fit ">
               <img
-                className='flex justify-end align-bottom object-contain object-right-bottom z-10'
+                className=' flex justify-end align-bottom object-contain object-right-bottom'
                 src={homePhoto}
                 alt="Matheus Correa"
                 />
@@ -43,7 +43,7 @@ export function Portfolio() {
 
 
         {/* quote */}
-        <div className='bg-zinc-700/10 w-full h-24'>
+        <div className='bg-zinc-800/10 w-full h-24'>
 
         </div>
 
